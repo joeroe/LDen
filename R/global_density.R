e5a7bd34bceb92d273d214757c1393aa38dbb338
Joe Roe <git@joeroe.io>
2025-03-14 10:29:16 +0100

    Infer area if not specified in global_density()

--- R/global_density.R
+++ R/global_density.R
@@ -4,14 +4,27 @@
 #' to the total size of the window or study region (e.g. the site or trench).
 #'
 #' @param x,y Equal-length numeric vectors of coordinates
-#' @param area Total area of the window
+#' @param area Total area of the window. If `NULL`, inferred as the area of the
+#'   bounding box of the coordinates specified by `x` and `y`.
 #'
 #' @return Number of points divided by area
 #' @export
 #'
 #' @examples
 #' global_density(AZ_A1020_BLM$x, AZ_A1020_BLM$y, area = 2409)
+#' global_density(AZ_A1020_BLM$x, AZ_A1020_BLM$y)
 global_density <- function(x, y, area = NULL) {
   stopifnot(length(x) == length(y))
+  if (is.null(area)) area <- area_bbox(x, y) # TODO: message with area?
   length(x) / area
+}
+
+#' Calculate area of the bounding box of a set of coordinates
+#'
+#' @noRd
+#' @keywords internal
+area_bbox <- function(x, y) {
+  stopifnot(is.numeric(x) & is.numeric(y))
+  stopifnot(all(!is.na(c(x, y)))) # TODO: or set na.rm below?
+  (max(x) - min(x)) * (max(y) - min(y))
 }
--- tests/testthat/test-global_density.R
+++ tests/testthat/test-global_density.R
@@ -1,8 +1,19 @@
 source(file = "../testdata.R")
 
-test_that("global density is calculated correctly", {
+test_that("global_density() calculates density correctly", {
   expect_equal(
     global_density(test_locs$x, test_locs$y, area = 10),
     nrow(test_locs) / 10
   )
+})
+
+test_that("global_density() infers area from bounding box if unspecified", {
+  expect_equal(
+    global_density(test_locs$x, test_locs$y),
+    nrow(test_locs) / area_bbox(test_locs$x, test_locs$y)
+  )
+})
+
+test_that("area_bbox() calculates area of bounding box correctly", {
+  expect_equal(area_bbox(c(0, 1), c(0, 1)), 1)
 })
